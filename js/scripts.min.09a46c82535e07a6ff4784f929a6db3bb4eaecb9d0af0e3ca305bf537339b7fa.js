8526458714c9f143e972307b73a0d40f694e1ddc
Jasper van Gelder <jasper@tulipsolutions.nl>
2019-11-10 13:18:23 +0000

    Automated build for branch 'master'
    Changes on source branch: 33bd5a88..6ce106d4.
    
    Source-Revision: 6ce106d444c99cbcc69cd09354668721dc2e7f5e
    Change-Id: I551c84d9fa677a96746e7b6142866eabfb6db9f6

--- js/scripts.min.09a46c82535e07a6ff4784f929a6db3bb4eaecb9d0af0e3ca305bf537339b7fa.js
+++ js/scripts.min.09a46c82535e07a6ff4784f929a6db3bb4eaecb9d0af0e3ca305bf537339b7fa.js
@@ -3,5 +3,3 @@ function formSubmitted(){submitted=true;}
 function contactFormSubmitLocation(location){if(typeof submitted!=='undefined'&&submitted){window.location=location;var targetElement=event.target||event.srcElement;gtag('event','email submitted',{'event_category':targetElement.alt,'event_label':'method'});}}
 function apiRegisterFocusEmail(){var targetElement=event.target||event.srcElement;gtag('event','Email focus',{'event_category':targetElement.alt,'event_label':'method'});}
 function onloadApiRegister(location){if(typeof submitted!=='undefined'&&submitted){window.location=location;var targetElement=event.target||event.srcElement;gtag('event','api registered',{'event_category':targetElement.alt,'event_label':'method'});}}
-window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}
-gtag('js',new Date());gtag('config','{{- .Site.Params.google_analytics_id -}}');gtag('config',{'anonymizeIp':true});
